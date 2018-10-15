125d01db7b014a4e52031e61b8fdade42deb49c4
nimrodshn <nshneor@redhat.com>
2018-10-15 10:32:11 +0300

    provisioning vars

--- run_container.sh
+++ run_container.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-aws_access_key_id=$(./get-aws-profile.sh --key)
-aws_secret_access_key=$(./get-aws-profile.sh --secret)
-aws_profile=opstest-c42b
-
+AWS_ACCESS_KEY_ID=$(./get-aws-profile.sh --key)
+AWS_SECRET_ACCESS_KEY=$(./get-aws-profile.sh --secret)
 INVENTORY_PATH=/home/nshneor/workspace/openshift-ansible-scripts/inventory.yml
 
 docker run -tu `id -u` \
        -v $HOME/.ssh/id_rsa:/opt/app-root/src/.ssh/id_rsa:Z,ro \
+       -v $HOME/workspace/openshift-ansible-scripts/tmp-provisioning-vars.yml:/usr/share/ansible/provisioning_vars.yml:Z,ro \
        -v /etc/ansible/hosts:$INVENTORY_PATH \
+       -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+       -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
        -e PLAYBOOK_FILE=playbooks/aws/openshift-cluster/provision_install.yml \
        -e INVENTORY_FILE=$INVENTORY_PATH \
-       -e AWS_ACCESS_KEY_ID=$aws_access_key_id \
-       -e AWS_SECRET_ACCESS_KEY=$aws_secret_access_key \
-       -e OPTS="-v" -t \
-       docker.io/openshift/origin-ansible:v3.11
+       -e OPTS="-vvv -e @/usr/share/ansible/provisioning_vars.yml" -t \
+       docker.io/openshift/origin-ansible:latest
